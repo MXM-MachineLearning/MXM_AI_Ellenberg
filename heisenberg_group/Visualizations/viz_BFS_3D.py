905f0e4d8988b08a0f84f8e80debf774a752fad5
Jimmy Vineyard <72844296+jwvineyard@users.noreply.github.com>
2023-10-30 15:41:34 -0500

    3d rotation plots.

--- heisenberg_group/Visualizations/viz_BFS_3D.py
+++ heisenberg_group/Visualizations/viz_BFS_3D.py
@@ -3,38 +3,41 @@ import pandas as pd
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-# adapted from https://www.geeksforgeeks.org/how-to-animate-3d-graph-using-matplotlib/?ref=lbp
-
 # Creating 3D figure
-fig = plt.figure(figsize = (8,8))
+fig = plt.figure(figsize=(8, 8))
 ax = plt.axes(projection='3d')
 
 df = pd.read_csv("../Data_Generation/Data_files/bfs_heisenberg_data.csv")
 
 # this filtering is necessary to limit the number of points we plot because otherwise this is slow
-df = df[df['num_steps']<10]
+df = df[df['num_steps'] < 10]
 
 # Create data
 x = df["val1"]
 y = df['val3']
 z = df['val2']
 
-first_move_A = df[df['last_matrix']==0]
-first_move_B = df[df['last_matrix']==1]
-first_move_C = df[df['last_matrix']==2]
-first_move_D = df[df['last_matrix']==3]
+first_move_A = df[df['last_matrix'] == 0]
+first_move_B = df[df['last_matrix'] == 1]
+first_move_C = df[df['last_matrix'] == 2]
+first_move_D = df[df['last_matrix'] == 3]
 
-ax.scatter(first_move_A["val1"], first_move_A["val2"], first_move_A["val3"], color="red")
-ax.scatter(first_move_B["val1"], first_move_B["val2"], first_move_B["val3"], color="green")
-ax.scatter(first_move_C["val1"], first_move_C["val2"], first_move_C["val3"], color="black")
-ax.scatter(first_move_D["val1"], first_move_D["val2"], first_move_D["val3"], color="blue")
+def plot_data():
+    ax.scatter(first_move_A["val1"], first_move_A["val2"], first_move_A["val3"], color="red")
+    ax.scatter(first_move_B["val1"], first_move_B["val2"], first_move_B["val3"], color="green")
+    ax.scatter(first_move_C["val1"], first_move_C["val2"], first_move_C["val3"], color="black")
+    ax.scatter(first_move_D["val1"], first_move_D["val2"], first_move_D["val3"], color="blue")
 
+# Define update function for animation
+def update(angle):
+    ax.cla()  # Clear axis
+    plot_data()  # Plot data
+    ax.view_init(45, angle)  # Update view angle
 
-# 360 Degree view
-for angle in range(0, 180):
+# Create animation
+ani = FuncAnimation(fig, update, frames=np.arange(0, 360, 2), blit=False)
 
-    ax.view_init(45, angle*2)
-    plt.draw()
-    plt.pause(.001)
- 
+# Save animation as GIF
+ani.save('rotation.gif', writer='imagemagick', fps=20)
+
 plt.show()
